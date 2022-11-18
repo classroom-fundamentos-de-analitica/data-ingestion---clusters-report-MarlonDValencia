e47596590d1ead3f0798f147234099427fbaf3c3
MarlonDValencia <mvalenciapa@unal.edu.co>
2022-11-17 21:38:15 -0500

    test finished

--- pregunta.py
+++ pregunta.py
@@ -11,7 +11,38 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
+import re
 
 def ingest_data():
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    return df
+  with open('clusters_report.txt') as text:
+    lines = text.readlines()
+  lines = lines[4:]
+
+  groups = []
+
+  
+  groups = [0, 0, 0, '']
+
+  for row in lines:
+
+    if re.match('^ +[0-9]+ +', row):
+      numero, valor, porcentaje, *txt = row.split()
+      group[0] = int(numero)
+      group[1] = int(valor)
+      group[2] = float(porcentaje.replace(',','.'))
+      txt.pop(0) 
+      txt = ' '.join(txt)
+      group[3] += txt
+
+    elif re.match('^\n', row) or re.match('^ +$', row):
+      group[3] = group[3].replace('.', '') 
+      groups.append(group)
+      group = [0, 0, 0, '']
+
+    elif re.match('^ +[a-z]', row):
+      txt = row.split()
+      txt = ' '.join(txt)
+      group[3] += ' ' + txt
+  df = pd.DataFrame (groups, columns = ['clusters', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+
+  return df
